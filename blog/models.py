e2ffb11dd6a214eb585214bb824f2318d27ce6e6
jiyuhosi <jiyuhosi@gmail.com>
2021-01-11 18:09:19 +0900

    blog app -post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Post(models.Model):
+    #title名 30文字まで
+    title = models.CharField(max_length=30)
+    #内容
+    content = models.TextField()
+    #作成日
+    created = models.DateTimeField()
+    #作成者
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
